1e9d0865534e1c92f455ed0ccd27bfb311e23bc7
CruzPame <cruzpame71@gmail.com>
2025-03-27 16:58:16 -0600

    se modificaron

--- remix.config.ts
+++ remix.config.ts
@@ -1,5 +1,6 @@
 /** @type {import('@remix-run/dev').AppConfig} */
-export default {
+module.exports = {
+  serverBuildTarget: "vercel",
   server: "./server.js", // Asegura que este archivo exista
   serverBuildPath: "build/server/index.js",
   serverDependenciesToBundle: "all",
