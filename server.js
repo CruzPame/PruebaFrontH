922d20fb043eb2341b92f7b64a6a4b477ee17856
CruzPame <cruzpame71@gmail.com>
2025-03-28 11:59:39 -0600

    ++ cambios

--- server.js
+++ server.js
@@ -2,3 +2,6 @@ import { createRequestHandler } from "@remix-run/node";
 import * as build from "./build/index.js";
 
 export default createRequestHandler({ build });
+
+// Asegura que Vercel use el manejador correcto
+module.exports = createRequestHandler({ build });
